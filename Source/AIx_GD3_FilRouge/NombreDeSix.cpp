454ff7127ea10b8f07575daf1ca1fbd106948295
AstreeSnow <145676494+AstreeSnow@users.noreply.github.com>
2024-10-15 17:02:19 +0200

    pas mal
    en gros
    C++

---
+++ Source/AIx_GD3_FilRouge/NombreDeSix.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "NombreDeSix.h"
+
+NombreDeSix::NombreDeSix()
+{
+}
+
+NombreDeSix::~NombreDeSix()
+{
+}
---
+++ Source/AIx_GD3_FilRouge/SixStats.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SixStats.generated.h"
+
+UCLASS()
+class AIX_GD3_FILROUGE_API ASixStats : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ASixStats();
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+
+
+
+
+	UFUNCTION(BlueprintCallable)
+	void InitializeStats(int32 dicenumber);
+};
